73e53df28dcd457bb186b876625a7ddb55bebcdd
Nate Kroll <kroll.nate@gmail.com>
2012-02-26 12:35:16 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+== Welcome to Generation on Rails
+
+	Vision: One stop shop for our agents to manage and grow their business.
+	
+	Overview of Features:
+		=> Listing Database
+		=> Client Relationship Management
+		=> Task Management and Scheduling
+		=> Resources {forms, agreements, required docs, etc}
+		=> Interactive Training Guide {to go along with in-person training sessions}
+		=> Accounting
+		=> Communication with MGMT
+	
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Generationchicago::Application.config.secret_token = '51275bfa04f33dd5496a75a2572377355c1c22782045cca4f1198c63a9acd970d8b9b3702f2ab5d3a37d878a35cf420b19cf5ebd6246d51baa6ec97f5104cb56'
